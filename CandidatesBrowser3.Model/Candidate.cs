bff01ac51c96480cd52050b72778d5b378dad980
Jacek Placek <jacekp1@op.pl>
2018-10-27 16:26:28 +0200

    Add project files.

---
+++ CandidatesBrowser3.Model/Candidate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandidatesBrowser3.Model
+{
+    public class Candidate : INotifyPropertyChanged
+    {
+        private int id;
+        public int ID
+        {
+            get { return id; }
+            set {
+                id = value;
+                RaisePropertyChange("ID");
+                }
+        }
+
+        private string firstName;
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                firstName = value;
+                RaisePropertyChange("FirstName");
+            }
+        }
+
+        public void RaisePropertyChange(string propertyName)
+        {
+            if(PropertyChanged!=null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+            
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
